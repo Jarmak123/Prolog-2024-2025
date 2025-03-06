480756dec932f2c8f1233ca6f4c6635a0a31e064
JRMK <147035765+Jarmak123@users.noreply.github.com>
2025-03-06 11:47:11 +0100

    Add files via upload

---
+++ klocki-2.pl
@@ -0,0 +1,24 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o układzie klocków
+% Definiowane predykaty:
+%           na/2
+%============================
+
+% na(X,Y)
+% opis: spełniony, gdy klocek x leży
+% bezpośrednio na klocku Y
+% -----------------------------------------------na/2
+           na(c,a).
+           na(c,b).
+           na(d,c).
+           pod(X,Y):-na(Y,X).
+% -----------------------------------------------na/2
+
+/*
+Informacje o budowie programu:
+Program składa się z 3 klauzul.
+Program zawiera 1 definicję relacji.
+Jest to relacja na/2.
+Defninicja relacji na/2 składa się z
+3 klauzul, które są faktami.
+*/
